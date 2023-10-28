00ed193f600fc59547503655680986f073204df0
olegatorh <herston4ik@gmail.com>
2023-10-28 11:25:27 +0300

    add new features to placess

--- front/src/components/home/DetailsScreeen.jsx
+++ front/src/components/home/DetailsScreeen.jsx
@@ -1,33 +1,46 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Button, Text, View} from "react-native";
+import {Button, FlatList, Image, Text, View} from "react-native";
 import {useRoute} from "@react-navigation/native";
 import axios from "axios";
 import {Context} from "../globalContext/globalContext";
+import styles from "./style";
 
 
 export const DetailsScreen = () => {
     const route = useRoute();
-    const globalContext = useContext(Context);
-    const otherProp = route.params.props;
-    const {token} = globalContext;
-    const headers = {"Authorization": `Token ${token}`}
-
-    console.log(otherProp)
-    const onClickR = () => {
-        axios.get(`http://192.168.88.254:8000/api/places/items/${otherProp['id']}`, {headers}).then(
-            (response) => {
-                console.log(response.data)
-            }
-        )
-    }
+    const {place, items} = route.params.props;
+    console.log(items)
     return (
         <View>
-            <Text>{otherProp['item_picture']}</Text>
-            <Text>{otherProp['tables']}</Text>
-            <Text>{otherProp['additional_info']}</Text>
-            <Text>MENU</Text>
-            <Button onPress={() => onClickR()} title={'check menu'}> </Button>
-            
+            <Image
+                style={styles.detail_image}
+                source={{uri: `http://192.168.88.254:8000${place['item_picture']}`}} // Replace with your image URL
+            />
+            <View style={styles.name_tables}>
+                <Text style={styles.place_name}>{place['place_name']}</Text>
+                <Text style={styles.tables}>available tables:{place['tables']}</Text>
+            </View>
+
+            <Text style={styles.additional_info}>{place['additional_info']}</Text>
+            <Button style={styles.order_button} title={'Order Table'}/>
+            <View style={{marginTop: 40}}>
+                <Text style={{fontSize: 20, textAlign: 'center', alignItems: 'center'}}>Menu</Text>
+                <FlatList
+                    data={items}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => (
+                        <View>
+                            <Text>{item.item_name}</Text>
+                            <Text>{item.additional_info}</Text>
+                            <Image
+                                source={{uri: `http://192.168.88.254:8000${item.item_picture}`}} // Replace with your image URL
+                            />
+                            <Text>{item.item_price}</Text>
+                        </View>
+                    )}
+                />
+
+            </View>
         </View>
     );
 }
--- front/src/components/home/RenderResult.jsx
+++ front/src/components/home/RenderResult.jsx
@@ -1,19 +1,42 @@
-import {Button, Image, Text, View} from "react-native";
+import {Button, FlatList, Image, Text, TouchableOpacity, View} from "react-native";
 import styles from "./style";
 import {useNavigation} from "@react-navigation/native";
+import axios from "axios";
+import {Context} from "../globalContext/globalContext";
+import {useContext} from "react";
 
 export const RenderResult = ({results}) => {
     const navigation = useNavigation()
+    const globalContext = useContext(Context);
+    const {token} = globalContext;
+    const headers = {"Authorization": `Token ${token}`}
+
+    const onClickR = (item) => {
+        axios.get(`http://192.168.88.254:8000/api/places/items/${item.id}`, {headers}).then(
+            (response) => {
+                navigation.navigate('Details', {name: item.place_name, props: {items: response.data, place: item}})
+            }
+        )
+    }
     return (
-        <View>
-            {results.map((place) => (
-                <View key={place['id']} style={styles.container}>
-                    <Button title={place['place_name']}
-                            onPress={() => navigation.navigate('Details', {name: place['place_name'], props: place})}>
-                        {/*<Image source={require(`./../../../../../djangoMobileAppBack/backend/media/${place['item_picture']}`)} />*/}
-                    </Button>
-                </View>
-            ))}
-        </View>
-    )
+        <FlatList
+            data={results}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => (
+                <TouchableOpacity onPress={() => onClickR(item)}>
+                    <View>
+                        <View style={styles.itemContainer}>
+                            <Image
+                                style={styles.image}
+                                source={{uri: `http://192.168.88.254:8000${item.item_picture}`}} // Replace with your image URL
+                            />
+                        </View>
+                        <Text style={styles.text}>{item.place_name}</Text>
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    );
 }
+
+
